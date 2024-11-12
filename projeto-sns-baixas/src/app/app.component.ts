5d96f66a0d50eab5214fbf122ed84ccf3a3ba9d3
mario <mario201santos11@gmail.com>
2024-11-12 20:01:29 +0000

    0.01

--- projeto-sns-baixas/src/app/app.component.ts
+++ projeto-sns-baixas/src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import {RouterLink, RouterOutlet} from '@angular/router';
-import {NgIf} from "@angular/common";
+import {NgClass, NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgIf, RouterLink],
+  imports: [RouterOutlet, NgIf, RouterLink, NgClass],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
